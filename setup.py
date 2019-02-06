409b2272cf7840d05fad0c0364e97a209cfb99cd
Sylvain Thénault <sylvain@sytweb.org>
2019-02-06 08:04:15 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 #
 # Copyright (c) 2018 by Sylvain Thénault sylvain@lowatt.fr
 #
-# This program is part of lowatt_collect.
+# This program is part of pdfss.
 #
-# lowatt_collect is free software: you can redistribute it and/or modify
+# pdfss is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# lowatt_collect is distributed in the hope that it will be useful,
+# pdfss is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with lowatt_collect.  If not, see <https://www.gnu.org/licenses/>.
+# along with pdfss.  If not, see <https://www.gnu.org/licenses/>.
 
 from setuptools import setup
 
@@ -24,10 +24,11 @@ setup(
     version='1.0',
     url='https://github.com/lowatt/lowatt_pdfss',
 
-    licence='GPL3',
+    license='GPL3',
     classifiers=[
         "Programming Language :: Python :: 3.6",
-        "License :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later "
+        "(GPLv3+)",
         "Operating System :: OS Independent",
     ],
     description='PDF scraping system',
