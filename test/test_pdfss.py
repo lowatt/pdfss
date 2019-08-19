24a3389a127762eb56763388493a61da548ddce2
Sylvain Thénault <sylvain@lowatt.fr>
2019-08-19 12:20:26 +0200

    [test] Improve test coverage by testing basic procesors

--- test/test_pdfss.py
+++ test/test_pdfss.py
@@ -42,6 +42,75 @@ class DumpPDFStructureTC(unittest.TestCase):
         )
 
 
+class fake_ltobj:
+    def __init__(self, text):
+        self.lower_text = text.lower()
+
+
+class DebugProcessorTC(unittest.TestCase):
+    def test(self):
+        state, ltobj = 'state', fake_ltobj('text data')
+
+        def gen():
+            sent_back = (yield state + '1', ltobj)
+            self.assertEqual(sent_back, (True, 'new state'))
+
+            sent_back = (yield state + '2', ltobj)
+            self.assertEqual(sent_back, (False, 'end state'))
+
+        processor = iter(pdfss.debug_processor(iter(gen()), {}))
+        with self.assertLogs('lowatt') as cm:
+
+            yielded = next(processor)
+            self.assertEqual(yielded, (state + '1', ltobj))
+
+            yielded = processor.send((True, 'new state'))
+            self.assertEqual(yielded, (state + '2', ltobj))
+
+            with self.assertRaises(StopIteration):
+                processor.send((False, 'end state'))
+
+        self.assertEqual(
+            cm.output,
+            [
+                "INFO:lowatt.pdfss:[state1] 'text data'",
+                "INFO:lowatt.pdfss:[state2] 'text data'"
+            ]
+        )
+
+
+class SimpleLtobjProcessorTC(unittest.TestCase):
+    def test(self):
+        state, ltobj = 'state', fake_ltobj('text data')
+
+        def gen():
+            sent_back = (yield state + '1', ltobj)
+            self.assertEqual(sent_back, (True, 'new state 1'))
+
+            sent_back = (yield state + '2', ltobj)
+            self.assertEqual(sent_back, (False, 'func state processed'))
+
+            sent_back = (yield state + '1', ltobj)
+            self.assertEqual(sent_back, (True, 'new state 2'))
+
+        def func(state, ltobj, data):
+            self.assertIn(state, ('state1', 'state2'))
+            if state == 'state1':
+                return False, 'func state recurs'
+            return True, 'func state processed'
+
+        processor = iter(pdfss.simple_ltobj_processor(func)(iter(gen()), {}))
+
+        yielded = next(processor)
+        self.assertEqual(yielded, ('func state recurs', ltobj))
+
+        yielded = processor.send((True, 'new state 1'))
+        self.assertEqual(yielded, ('func state recurs', ltobj))
+
+        with self.assertRaises(StopIteration):
+            processor.send((True, 'new state 2'))
+
+
 def load_tests(loader, tests, ignore):
     tests.addTests(
         doctest.DocTestSuite('pdfss',
